dc88d57e252bbdb0e811b83cfe673a35ccd578a5
Sil van Diepen <me@silvandiepen.com>
2019-12-16 21:49:58 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+import "hv-input-field";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  message = "Angular Example";
+}
